c5fc76e23f087cde4301035a888ac3f80409fd19
bhecra <ingchristianburbano@gmail.com>
2018-09-22 03:25:16 -0500

    Update Planets

--- StarWars/script.js
+++ StarWars/script.js
@@ -17,14 +17,11 @@ function getPeliculas(){
         "https://bit.ly/2oRLOMx",
     ]
     peticion.onreadystatechange = function(){
-        document.getElementById('homeimg').style.display = 'none'
         if (this.readyState == 4 && this.status == 200) 
         {
             peliculas = JSON.parse(this.responseText)
-           
             for(i=0; i<peliculas.results.length; i++)
             {
-                console.log("pelicula"+(i+1)) 
                 datosTxt += "<div  class=\"row\"><div class=\"col-md-6 col-lg-6 text-center\">"+
                             "<img class=\"rounded-circle\" id=\"imgprofile\" style=\"width: 30%;\" src="+ portadaPeliculas[i] +" alt=\"\"></div>"+
                             "<div class=\"col-md-6 col-lg-6\">"+
@@ -35,30 +32,22 @@ function getPeliculas(){
                             "</div></div><hr>" 
 
             }
-            document.getElementById('films').innerHTML = datosTxt;
+            if(document.getElementById('films'))
+            {
+                document.getElementById('films').innerHTML = datosTxt;
+            }
+            
         }
     } 
     peticion.open('GET',www+'films/',true);
     peticion.send();
 }
 
-function renderPerson(url){
-    var peticion  = new  XMLHttpRequest();
-    
-    peticion.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200) {
-            person = JSON.parse(this.responseText)
-            console.log(person.name)
-            datosPerson += "<p>"+ "<strong>Nombre: </strong>" + person.name +"</p>"
+getPeliculas();
+characters();
 
-        }
-        document.getElementById('films').innerHTML = datosPerson;
-    }
-    peticion.open('GET',url,true);
-    peticion.send();
-}
 
-function chatacters(){
+function characters(){
     var peticion  = new  XMLHttpRequest();
     var perfil = [
         "https://bit.ly/2x5RXIq",
@@ -72,7 +61,7 @@ function chatacters(){
         "https://bit.ly/2QipvvL",
         "https://bit.ly/2Mj8min"
     ]
-    document.getElementById('homeimg').style.display = 'none'
+    
     peticion.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200) {
             person = JSON.parse(this.responseText)
@@ -89,25 +78,139 @@ function chatacters(){
             } 
             
         }
-        document.getElementById('films').innerHTML = datosPerson;
+        if(document.getElementById('person'))
+        {
+            document.getElementById('person').innerHTML = datosPerson;
+        }
+        
     }
     peticion.open('GET',www+'people/',true);
     peticion.send();
 }
 
 
-function getRequestGit(){
-    var peticion  = new  XMLHttpRequest();
-    peticion.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200) {
-            datos = JSON.parse(this.responseText)
-            console.log(datos);
-            document.getElementById('title').innerHTML ="<title> Perfil | "+datos.login+"</title>"
+var allData = [];
+var planetas = [
+    'https://bit.ly/2MVgwO8',
+    'https://bit.ly/2pvc7Zb',
+    'https://bit.ly/2DjitVf',
+    'https://bit.ly/2NNAwqy',
+    'https://bit.ly/2O0mjqj',
+    'https://bit.ly/2DmeZRW',
+    'https://bit.ly/2I3r2C9',
+    'https://bit.ly/2PW5Dxs',
+    'https://bit.ly/2xwZGjY',
+    'https://bit.ly/2pbSPYq',
+    'https://bit.ly/2pr05zX',
+    'https://bit.ly/2pwdpmt',
+    'https://bit.ly/2Nye3hS',
+    'https://bit.ly/2DnDzSl',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+];
+var cardPlanet = "";
+
+function getUrls(url)
+{
+    var httpR = new XMLHttpRequest();
+    httpR.onload = function()
+    {
+        var i = 0;
+        datos = JSON.parse(this.responseText)
+        //console.log(datos)
+        if(datos.next)
+        {
+            getUrls(datos.next)
+            allData.push(datos.results)
+        }
+        else{
+            allData.push(datos.results)
+            allData.forEach(function(pages){
+                pages.forEach(function(planets){
+                    cardPlanet += "<div class=\"col-sm-6 col-lg-4 mt-2\">"+
+                                    "<div class=\"card\">"+
+                                        "<img class=\"card-img-top width=100%;\" src=\""+planetas[i]+"\" alt=\"Card image cap\">"+
+                                        "<div class=\"card-body\">"+
+                                            "<h4 class=\"card-title\">"+planets.name+"</h4>"+
+                                            "<p class=\"card-text\">"+
+                                            "<p>Clima:"+ planets.climate +"</p>"+
+                                            "<p>Terreno:"+ planets.terrain+"</p>"+
+                                            "<p>Gravity:"+ planets.gravity+"</p>"+
+                                            
+                                            "</p>"+
+                                            "<a href=\"#!\" class=\"btn btn-primary\">Ver Detalles</a>"+
+                                        "</div>"+
+                                    "</div>"+
+                                 "</div>"
+                    i += 1;
+                });
+            });
             
-            renderRepo(datos.repos_url);
+        }
+            /*urls.forEach(function(url){
+                planetas.push(getPlanets(url));
+            }
+            );
+
+            Promise.all(planetas)
+            .then(function(data){
+                console.log(data)
+                data.forEach(function(page){
+                    
+                    page.forEach(function(element){
+                        console.log(element.name)
+                    });
+                    
+                });
+                /* for(var i=0; i<data.length; i++)
+                {
+
+                }
+                console.log(data) 
+
+            }) 
+        }*/
+        if(document.getElementById('planetsCard'))
+        {
+            document.getElementById('planetsCard').innerHTML = cardPlanet;
         }
     }
-    peticion.open('GET','https://api.github.com/users/'+inputText.value,true);
-    peticion.send();
-}
+    httpR.open('GET',url,true);
+    httpR.send();
 
+}
+getUrls("https://swapi.co/api/planets/");
+
+function getPlanets(url)
+{
+    return new Promise(function(resolve, reject)
+    {
+        var httpR = new XMLHttpRequest();
+        httpR.onload = function()
+        {
+            response  = JSON.parse(this.responseText).results
+            resolve(response)
+        }
+        httpR.onerror = function()
+        {
+            reject(Error('Hay un error'))
+        }
+        httpR.open('GET',url,true);
+        httpR.send();
+    }
+    );
+}
