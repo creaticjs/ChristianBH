dda4f771c78510e325ccc01f3b7c9ec35dc3447c
bhecra <ingchristianburbano@gmail.com>
2018-10-01 17:36:36 -0500

    Update pokemon

--- Pokemon/peticion.js
+++ Pokemon/peticion.js
@@ -1,21 +1,32 @@
 //https://pokeapi.co/api/v2/pokemon/
 urlPokemons = [];
+var pokeCard ='';
+var contenido = document.getElementById('contenido');
+var i = 0;
+var datos1 = [];
+var color = [];
 
-function peticion(url,functionDone, functionFail){
-    $.ajax({
-        type:"get",
-        url:url,
-        dataType:"json",
-    })
-    .done(function(data){
-        functionDone(data)
-    })
-    .fail(function(err){
-        functionFail(err)
+
+function getPeticion(url){
+    return new Promise(function(resolve,reject){
+        $.ajax({
+            type:"get",
+            url:url,
+            dataType:"json",
+        })
+        .done(function(data){
+            resolve(data)
+        })
+        .fail(function(err){
+            reject(err)
+        })
     })
 }
 
-peticion("https://pokeapi.cO/api/v2/pokemon/",functionDone,functionFail);
+getPeticion("https://pokeapi.cO/api/v2/pokemon/")
+.then(functionDone)
+
+
 
 function functionDone(data)
 {
@@ -24,30 +35,76 @@ function functionDone(data)
         urlPokemons.push(pokemon.url);
     });
 
-    peticion(urlPokemons[0], getDataPokemon, functionFail)
+    Promise.all(urlPokemons)
+    .then(function(urlPoke){
+        urlPoke.forEach(poke => {
 
-    /* urlPokemons.forEach(url => {
+            getPeticion(poke)
+            .then(function(data){
+                console.log("CARGANDO...")
+                if(datos1.length < urlPokemons.length-2)
+                {
+                    datos1.push(data)
+                }
+                else
+                {
+                    console.log("diferente")
+                    datos1.forEach(data => {
 
-        peticion(url, getDataPokemon, functionFail)
-        
-    }); */
-}
-function getDataPokemon(data)
-{
-    var dataPokeTxt = ''
-
-    dataPokeTxt = ''
-    //document.getElementById('contenido').innerHTML = dataPokeTxt
-    console.log(data.name)
-    console.log(data.weight)
-    console.log(data.sprites.front_default)
-    data.abilities.forEach(abiliti => {
-        console.log(abiliti.ability.name)
+                        getPeticion(data.species.url)
+                        .then(function(specie){
+                            if(color.length < datos1.length -2)
+                            {
+                                console.log(color.length)
+                                color.push(specie.color.name)
+                                renderTarget(data,specie.color.name)
+                            }
+                            else
+                            {
+                                contenido.innerHTML = pokeCard;
+                                $("#cargando").hide()
+                                $("#contenido").show()
+                            }
+                        })
+                    });
+                }
+            
+            })
+        })
     })
+        
+    
+ 
+}
+            /* 
+            .then(function(data){
+            }) */
+            /* getPeticion(poke)
+            .then(function(data){
+                getPeticion(data.species.url)
+                .then(function(specie){
+                    
+                    renderTarget(data,specie.color.name)        
+    
+                })
+                
+            }) */
+            
+        
+        //getSpecies(datos1)
 
-    peticion(data.species.url,species,functionFail)
+        
     
-    
+    /*  */
+
+
+function getSpecies(data)
+{
+    console.log(data[0].name)
+    /* data.forEach(poke =>{
+        
+        console.log(poke)
+    }) */
 }
 
 function functionFail(ERR)
@@ -55,7 +112,43 @@ function functionFail(ERR)
     console.log('Error')
 }
 
-function renderTarget(data)
+function renderTarget(data, color)
 {
+   
+        var abil = '';
+        var color = color;
+        var colorWord = 'black';
 
+        if(color != 'white')
+        {
+            colorWord = 'white';
+        }
+    
+        data.abilities.forEach(element =>{
+            abil += `<li> ${element.ability.name} </li>`
+        })
+    
+        pokeCard +=`<div class="col-lg-3 col-sm-6 mt-5">
+            <div class="d-flex justify-content-center">
+                <div id="tarjet-out">
+                    <div id="tarjet-in" style="background-image: url(../img/${color}.jpg); color:${colorWord};">
+                        <h4 class="text-center">${data.name}</h4>
+                        <div class="img-bg">
+                            <img id="poke-img" src=${data.sprites.front_default} alt="Card image">
+                        </div>
+                        <div class="justify-text">
+                            <p>Peso: ${data.weight} kg</p>
+                            
+                            <p>Habilidades:  <small> <br>
+                            ${abil}
+                            </small></p>
+                            <p class="card-text">This Pokémon's Speed is <br> doubled during strong sunlight..</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    
+        
+    
 } 
--- Pokemon/species.js
+++ Pokemon/species.js
@@ -1,19 +1,6 @@
 //https://pokeapi.co/api/v2/pokemon-species/
 
 
-function species(specie)
-{  
-    specie.flavor_text_entries.forEach(text => {
-       
-        if(text.language.name == 'es')
-        {
-            console.log(text.flavor_text);     
-        }  
-        
-    });
-    peticion(specie.evolution_chain.url,evolution,functionFail)
-}
-
 function evolution(evolution)
 {
     var evolves = evolution.chain.evolves_to;
