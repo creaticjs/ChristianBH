554cbb57a7a21d151fd37b9c7eb6daa361d69d1c
bhecra <ingchristianburbano@gmail.com>
2018-10-01 20:36:06 -0500

    Optimized2 Pokemon

--- Pokemon/peticion.js
+++ Pokemon/peticion.js
@@ -23,12 +23,9 @@ function getPeticion(url){
     })
 }
 
-
-/* getPeticion("https://pokeapi.co/api/v2/pokemon/:id")
-.then(functionDone) */
 poke = 'https://pokeapi.co/api/v2/pokemon/:id/'
 
-for (var i= 1; i<500; i++)
+for (var i= 1; i<300; i++)
 {
     pokeUrl = poke.replace(":id",i) 
     urlPokemons.push(getPeticion(pokeUrl));
@@ -39,14 +36,11 @@ Promise.all(urlPokemons)
 
 function functionDone(dataBase)
 {
-    console.log(dataBase)
     dataBase.forEach(data => {
-
         getPeticion(data.species.url)
         .then(function(specie){
             if(color.length < dataBase.length-1)
             {
-                console.log(color.length)
                 color.push(specie.color.name)
                 renderTarget(data,specie.color.name)
             }
@@ -55,119 +49,17 @@ function functionDone(dataBase)
                 $("#cargando").hide()
                 contenido.innerHTML = pokeCard;
                 $("#contenido").show()
-
-                
+  
             }
         })
     });
-            /* 
-            .then(function(data){
-                console.log("CARGANDO...")
-                //console.log(datos1.length)
-                //console.log(urlPokemons.length)
-                if(datos1.length < urlPokemons.length-1)
-                {
-                    datos1.push(data)
-                }
-                else
-                {
-                    console.log("diferente")
-                    datos1.forEach(data => {
-
-                        getPeticion(data.species.url)
-                        .then(function(specie){
-                            if(color.length < datos1.length-1)
-                            {
-                                console.log(color.length)
-                                color.push(specie.color.name)
-                                renderTarget(data,specie.color.name)
-                            }
-                            else
-                            {
-                                $("#cargando").hide()
-                                contenido.innerHTML = pokeCard;
-                                $("#contenido").show()
-                            }
-                        })
-                    });
-                }
-            
-            })
-         */
-    
         
-    
- 
 }
-
-/* Promise.all(urlPokemons)
-    .then(function(urlPoke){
-        urlPoke.forEach(poke => {
-
-            getPeticion(poke)
-            .then(function(data){
-                console.log("CARGANDO...")
-                console.log(datos1.length)
-                console.log(urlPokemons.length)
-                if(datos1.length < urlPokemons.length)
-                {
-                    datos1.push(data)
-                }
-                else
-                {
-                    console.log("diferente")
-                    datos1.forEach(data => {
-
-                        getPeticion(data.species.url)
-                        .then(function(specie){
-                            if(color.length < datos1.length)
-                            {
-                                console.log(color.length)
-                                color.push(specie.color.name)
-                                renderTarget(data,specie.color.name)
-                            }
-                            else
-                            {
-                                $("#cargando").hide()
-                                contenido.innerHTML = pokeCard;
-                                $("#contenido").show()
-                            }
-                        })
-                    });
-                }
-            
-            })
-        })
-    }) */
-            /* 
-            .then(function(data){
-            }) */
-            /* getPeticion(poke)
-            .then(function(data){
-                getPeticion(data.species.url)
-                .then(function(specie){
-                    
-                    renderTarget(data,specie.color.name)        
-    
-                })
-                
-            }) */
-            
-        
-        //getSpecies(datos1)
-
-        
-    
-    /*  */
 
 
 function getSpecies(data)
 {
     console.log(data[0].name)
-    /* data.forEach(poke =>{
-        
-        console.log(poke)
-    }) */
 }
 
 function functionFail(ERR)
@@ -210,8 +102,6 @@ function renderTarget(data, color)
                     </div>
                 </div>
             </div>
-        </div>`
-    
-        
+        </div>`    
     
 } 
